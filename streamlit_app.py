3c42f8f42950a61b0ae7caa6aef089fa2c2bc9e5
Brent Katz <121365473+bksflake@users.noreply.github.com>
2022-12-26 12:32:00 -0600

    Update streamlit_app.py
    corrected some of the menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Smoothie')
-streamlit.text('🐔 Eggs')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 
