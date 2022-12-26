0c94d45e97dddd5a2479d29930a54fdcc97f1e49
Brent Katz <121365473+bksflake@users.noreply.github.com>
2022-12-26 12:24:31 -0600

    Update streamlit_app.py
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Eggs')
