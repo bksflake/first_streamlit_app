1e372725d6c1a0fb59db27d5c920ca9ff0248d2d
Brent Katz <121365473+bksflake@users.noreply.github.com>
2022-12-26 12:28:34 -0600

    Update streamlit_app.py
    added BYO smoothie section

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Smoothie')
 streamlit.text('🐔 Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
